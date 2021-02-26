d0aef346a33a81b697fed89353b6d2974c7c34c9
sk521 <samkwon521@gmail.com>
2021-02-26 16:30:30 -0500

    styling navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -9,19 +9,53 @@ export const Navigation = () => {
         <li className="left">
           <ul>
             <li className="icon">
-              <span className="main">
-                <Github />
-              </span>
+              <a
+                href="https://github.com/sk521"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="main">
+                  <Github />
+                </span>
+              </a>
             </li>
             <li className="icon">
-              <span className="main">
-                <LinkedIn />
-              </span>
+              <a
+                href="https://www.linkedin.com/in/samuel-kwon/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="main">
+                  <LinkedIn />
+                </span>
+              </a>
             </li>
             <li className="icon">
-              <span className="main">
-                <Medium />
-              </span>
+              <a
+                href="https://medium.com/@samkwon521"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="main">
+                  <Medium />
+                </span>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li className="right">
+          <ul>
+            <li>
+              Home
+            </li>
+            <li>
+              About
+            </li>
+            <li>
+              Work
+            </li>
+            <li>
+              Contact
             </li>
           </ul>
         </li>
