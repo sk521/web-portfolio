2e1e5fbcae4cfdb0044901bb5f8ab43ba4e68efe
sk521 <samkwon521@gmail.com>
2021-03-02 00:29:01 -0500

    nav bar left icons styled

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,22 +1,64 @@
 import React from "react";
+import styled from "styled-components";
 import { Github, LinkedIn, Medium } from "../assets/svg/icons"
+
+
+const Nav = styled.nav`
+  background-color: #201F1F;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 300
+`;
+
+const Container = styled.ul`
+  display: flex;
+  flex-direction: row;
+  max-width: 1920px;
+  width: 80%;
+  margin: auto;
+  text-transform: uppercase;
+  font-size: 20px;
+  justify-content: space-between;
+  font-weight: 700;
+  padding: 20px 0px;
+`;
+
+const Left = styled.li`
+  display: inline-block;
+`;
+
+const LeftContainer = styled.ul`
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 100%;
+`;
+
+const Icon = styled.span`
+  fill: #3A8370;
+`;
+
+
 
 export const Navigation = () => {
   return (
     <>
-    <nav className="navigation">
-      <ul className="container">
-        <li className="left">
-          <ul>
+    <Nav>
+      <Container>
+        <Left>
+          <LeftContainer>
             <li className="icon">
               <a
                 href="https://github.com/sk521"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <span className="main">
+                <Icon>
                   <Github />
-                </span>
+                </Icon>
               </a>
             </li>
             <li className="icon">
@@ -25,9 +67,9 @@ export const Navigation = () => {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <span className="main">
+                <Icon>
                   <LinkedIn />
-                </span>
+                </Icon>
               </a>
             </li>
             <li className="icon">
@@ -36,13 +78,13 @@ export const Navigation = () => {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <span className="main">
+                <Icon>
                   <Medium />
-                </span>
+                </Icon>
               </a>
             </li>
-          </ul>
-        </li>
+          </LeftContainer>
+        </Left>
         <li className="right">
           <ul>
             <li>
@@ -59,8 +101,8 @@ export const Navigation = () => {
             </li>
           </ul>
         </li>
-      </ul>
-    </nav>
+      </Container>
+    </Nav>
     </>
   )
 }
